8848b72529cc700800a03966ec45877a8a6d4c43
WangSuhang123 <1174445894@qq.com>
2024-12-13 16:46:05 +0800

    program update

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-# 这是一个示例 Python 脚本。
-
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+# 导入必要模块
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtGui import QPixmap
+from testmuyu import Ui_MainWindow  # 导入生成的 UI 类
 
 
-# 按间距中的绿色按钮以运行脚本。
+# 定义主窗口类
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.num = 0
+
+        # 初始化界面
+        self.numLabel.setText(str(self.num))
+        self.UploadImage.clicked.connect(self.uploadImage)
+        self.HitOnes.clicked.connect(self.hitOnes)
+        self.MakeZero.clicked.connect(self.makeZero)
+
+    # 上传图片功能
+    def uploadImage(self):
+        imagePath = QtWidgets.QFileDialog.getOpenFileName(self, "上传图片", "", "Image Files (*.jpg *.png *.jpeg)")[0]
+        if imagePath:
+            pixmap = QPixmap(imagePath)
+            self.Imagelabel.setPixmap(pixmap)
+            self.Imagelabel.setScaledContents(True)  # 图片自适应标签大小
+
+    # 点击 "敲一敲" 按钮，计数加1
+    def hitOnes(self):
+        self.num += 1
+        self.numLabel.setText(str(self.num))
+
+    # 点击 "清零" 按钮，计数重置为0
+    def makeZero(self):
+        self.num = 0
+        self.numLabel.setText(str(self.num))
+
+
+# 主程序入口
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    import sys
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    app = QtWidgets.QApplication(sys.argv)
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.exec_())
--- opencvtest.py
+++ opencvtest.py
@@ -5,7 +5,7 @@ cap = cv2.VideoCapture(0)
 while(True):
     ret, frame = cap.read()
     cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('w'):
         break
 
 #释放摄像头
